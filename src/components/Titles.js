83efc883c8d89387611591e34796f1daccae61fb
esteloabell <manitobaaa@gamil.com>
2020-02-27 17:19:46 -0600

    done

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Titles = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <p className="title-container__subtitle">Find out temperature, condition and more...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Titles;
